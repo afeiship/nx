24e224146cfcc5799d611d22c97b3b382a6a3dbb
afeiship <1290657123@qq.com>
2022-07-30 15:58:02 +0800

    feat: nx hi method

--- nx.go
+++ nx.go
@@ -5,8 +5,12 @@ import "fmt"
 var VERSION = "0.0.1"
 
 func Log(args ...interface{}) {
-	for _, param := range args {
-		fmt.Printf("%+v ", param)
-	}
-	fmt.Println("")
+  for _, param := range args {
+    fmt.Printf("%+v ", param)
+  }
+  fmt.Println("")
+}
+
+func Hi() {
+  fmt.Printf("Hello go cli")
 }
