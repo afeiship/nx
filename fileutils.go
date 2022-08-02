e90fbdba3e87c1fe6516cc1448a39244ad194e68
afeiship <1290657123@qq.com>
2022-08-02 21:45:39 +0800

    feat: add fileutils

---
+++ fileutils.go
@@ -0,0 +1,73 @@
+package nx
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+func WriteFile(fileName string, data []byte) error {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadFile(fileName string) ([]byte, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func Exists(fileName string) bool {
+	_, err := os.Stat(fileName)
+	return err == nil
+}
+
+func Remove(fileName string) error {
+	return os.Remove(fileName)
+}
+
+func Rename(oldName, newName string) error {
+	return os.Rename(oldName, newName)
+}
+
+func Mkdir(dirName string) error {
+	return os.Mkdir(dirName, 0755)
+}
+
+func ReadDir(dirName string) ([]os.FileInfo, error) {
+	return ioutil.ReadDir(dirName)
+}
+
+func IsFile(fileName string) bool {
+	fi, err := os.Stat(fileName)
+	if err != nil {
+		return false
+	}
+	return !fi.IsDir()
+}
+
+func IsDir(fileName string) bool {
+	fi, err := os.Stat(fileName)
+	if err != nil {
+		return false
+	}
+	return fi.IsDir()
+}
