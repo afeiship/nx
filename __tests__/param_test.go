dae53be6f358e894d6b4bd86c9454f046f6256d7
afeiship <1290657123@qq.com>
2024-10-30 10:54:06 +0800

    feat: add baseURL full test

--- __tests__/param_test.go
+++ __tests__/param_test.go
@@ -16,5 +16,18 @@ func TestParamData(f *testing.T) {
 	res := nx.Param(datamap)
 	if strings.Contains(res, "age=25") || strings.Contains(res, "name=afei") {
 		f.Log("TestParamData pass")
+		f.Log(res)
+	}
+}
+
+func TestParamWithBaseURL(f *testing.T) {
+	datamap := make(map[string]any)
+	datamap["name"] = "afei"
+	datamap["age"] = 25
+	baseURL := "https://www.example.com"
+	res := nx.Param(datamap, baseURL)
+	if strings.Contains(res, "age=25") || strings.Contains(res, "name=afei") || strings.Contains(res, "https://www.example.com") {
+		f.Log("TestParamWithBaseURL pass")
+		f.Log(res)
 	}
 }
