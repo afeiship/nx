f42a5d9d9d13e2dfccc9c3f886a15bd9992a30b9
afeiship <1290657123@qq.com>
2024-11-14 22:07:31 +0800

    feat: add isCmdExists

--- lib/shell.go
+++ lib/shell.go
@@ -10,10 +10,21 @@ func RunShell(command string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...) // 第一个元素是命令，后面的元素是参数
+
+	cmdName := cmdArgs[0]
+	if !IsCommandExists(cmdName) {
+		return "", nil
+	}
+
+	cmd := exec.Command(cmdName, cmdArgs[1:]...) // 第一个元素是命令，后面的元素是参数
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", err
 	}
 	return string(out), nil
+}
+
+func IsCommandExists(name string) bool {
+	_, err := exec.LookPath(name)
+	return err == nil
 }
